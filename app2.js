776c509f4b279e73244ed1ec9225003495aafcca
Ehar Kala <eharkala@ut.ee>
2021-12-11 15:16:45 +0200

    database.js added to gitignore

--- app2.js
+++ app2.js
@@ -91,7 +91,8 @@ app.post('/posts', async (req, res) => {
         const post = req.body;
         console.log(post);
         const newpost = await pool.query(
-            "INSERT INTO posts(title, body, urllink) values ($1, $2, $3) RETURNING * ", [post.title, post.body, post.urllink]
+            "INSERT INTO posts(title, body, urllink) values ($1, $2, $3) RETURNING * ", 
+            [post.title, post.body, post.urllink]
         );
         res.redirect('posts');
     } catch (err) {
